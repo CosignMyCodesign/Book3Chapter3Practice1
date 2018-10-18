ac5df6e58ff6f658041826c52e995fc4dec270bf
CosignMyCodesign <dillonwilliamsail@gmail.com>
2018-10-18 15:19:35 -0500

    Added a new way of doing this by inserting the content into the DOM

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
     console.log("chickenMonkey") // chickenmonkey will appear at every number divisible by 5 and 7
 }
  else if (currentNumber % 5 === 0) {
-      console.log("chicken") // Chicken will appear at every multiple of 5
+    console.log("chicken") // Chicken will appear at every multiple of 5
   }
   else if (currentNumber % 7 === 0) {
     console.log("monkey") // Monkey will appear at every multiple of 7
@@ -14,3 +14,22 @@ for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
   }
 }
 // This was fun!
+
+// OR you can do it by inserting the content into the DOM instead of console logging
+let paragraph = document.getElementsByTagName("p")[0]
+let text = "";
+
+for (let num = 1; num <= 100; num++){
+ if ((num % 5 === 0 ) && (num % 7 == 0)) {
+   text += "ChickenMonkey ";
+ } else if (num % 5 == 0 ) {
+   text += "Chicken "
+ } else if (num % 7 == 0 ) {
+   text += "Monkey "
+ } else {
+   text += `${num}` 
+ }
+}
+
+paragraph.textContent = text
+
